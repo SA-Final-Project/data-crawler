9fe8ba8ea1a847b494fbb3075a9369dc46ec11cf
Eyouel <eyouelaya@gmail.com>
2024-02-04 17:34:36 -0600

    Completed RTDS service

--- RTDS/src/main/java/com/rtds/RTDS/domain/RTDIS.java
+++ RTDS/src/main/java/com/rtds/RTDS/domain/RTDIS.java
@@ -7,6 +7,10 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -14,18 +18,23 @@ import java.util.concurrent.TimeUnit;
 @Component
 public class RTDIS {
 
-    @Value("${api_url}")
+//    @Value("${api_url}")
+//    private  String API_URL;
     private  String API_URL;
     
-    @Value("${topic_name}")
-    private  String KAFKA_TOPIC;
+//    @Value("${topic_name}")
+//    private  String KAFKA_TOPIC;
+    private String KAFKA_TOPIC;
 
     @Autowired
     private KafkaTemplate<String,String> kafkaProducer;
     private ScheduledExecutorService scheduler;
 
     // Start streaming process
-    public void startStreaming() {
+    public void startStreaming(String api) {
+        API_URL = api;
+        KAFKA_TOPIC = "RTD_"+extractApiName(API_URL);
+        System.out.println(KAFKA_TOPIC);
         scheduler = Executors.newSingleThreadScheduledExecutor();
         // Schedule the stream method to run every 10 seconds
         scheduler.scheduleAtFixedRate(this::stream, 0, 10, TimeUnit.SECONDS);
@@ -49,6 +58,14 @@ public class RTDIS {
                 });
     }
 
-
+    public static String extractApiName(String urlString) {
+try {
+            URL url = new URL(urlString);
+    return url.getHost();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
