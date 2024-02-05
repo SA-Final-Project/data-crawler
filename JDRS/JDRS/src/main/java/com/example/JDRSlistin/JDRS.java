25f0fdfb80b2f8e73325d0c9368258636502e5b5
Abel G/alef <abelgalef@gmail.com>
2024-02-05 10:58:46 -0600

    split the JDRS and value extractor

--- JDRS/JDRS/src/main/java/com/example/JDRSlistin/JDRS.java
+++ JDRS/JDRS/src/main/java/com/example/JDRSlistin/JDRS.java
@@ -1,6 +1,5 @@
 package com.example.JDRSlistin;
 
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -9,15 +8,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
-
-@Service
+@Component
 public class JDRS {
     private static final String OUTPUT_TOPIC_PREFIX = "RTDI_";
 
@@ -26,59 +27,18 @@ public class JDRS {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @KafkaListener(topics = "RTD")
-    public void processJsonMessage(ConsumerRecord<String, String> record) {
+    @KafkaListener(topicPattern = "RTD_.*")
+    public void processJsonMessage(ConsumerRecord<String, String> record, @Header()) {
         String json = record.value();
 
         try {
-            extractNumericValues(json);
+            List<String[]> numberValues = JSONNumberValueExtractor.extractNumericValues(json);
+            numberValues.forEach(values -> publishDataElement(values[0], values[1]));
         } catch (Exception e) {
             logger.error("Error processing JSON: " + e.getMessage());
         }
     }
 
-
-
-    public Map<String, Number> extractNumericValues(String jsonString) {
-        Map<String, Number> numericValues = new HashMap<>();
-
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode rootNode = objectMapper.readTree(jsonString);
-
-            extractNumericValuesFromNode(rootNode, "", numericValues);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return numericValues;
-    }
-
-    private void extractNumericValuesFromNode(JsonNode node, String currentPath, Map<String, Number> numericValues) {
-        Iterator<Map.Entry<String, JsonNode>> fields = node.fields();
-        while (fields.hasNext()) {
-            Map.Entry<String, JsonNode> entry = fields.next();
-            String fieldName = entry.getKey();
-            JsonNode fieldValue = entry.getValue();
-            String fullPath = currentPath.isEmpty() ? fieldName : currentPath + "." + fieldName;
-
-            if (fieldValue.isNumber()) {
-                numericValues.put(fullPath, fieldValue.numberValue());
-                publishDataElement(fullPath, fieldValue.numberValue().toString());
-            } else if (fieldValue.isObject()) {
-                extractNumericValuesFromNode(fieldValue, fullPath, numericValues);
-
-            }
-            else if (fieldValue.isArray()) {
-                for (JsonNode field : fieldValue) {
-                    extractNumericValuesFromNode(field, fullPath, numericValues);
-                }
-            }
-        }
-    }
     private void publishDataElement(String name, String value) {
         String topic = OUTPUT_TOPIC_PREFIX + name;
         kafkaTemplate.send(topic, name, value);
