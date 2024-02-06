ab5a6cd86c527628fcfcb368098e57d21406e7b4
kal <kalkidan.alemu@miu.edu>
2024-02-06 16:09:22 -0600

    Fix CDS service to accept from jdrs and send topics

--- ChangeDetectorServices/change-detector-service/src/main/java/edu/miu/changedetectorservice/service/impl/StandardDeviationServiceIml.java
+++ ChangeDetectorServices/change-detector-service/src/main/java/edu/miu/changedetectorservice/service/impl/StandardDeviationServiceIml.java
@@ -14,17 +14,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class StandardDeviationServiceIml implements StandardDeviationService {
 
-    @Value("${kafka.topics.cd.output}")
-    private String CHANGE_DETECTED_TOPIC;
-    private final KafkaTemplate<String, Integer> kafkaTemplate;
+
+    @Value("${kafka.topics.dis.input}")
+    private String INPUT_TOPIC;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void test(){
-        System.out.println(CHANGE_DETECTED_TOPIC);
+        System.out.println(INPUT_TOPIC);
     }
 
     @Override
     public void findDataChange(Long x) {
-
         if (InMemoryStorage.VALUE_HOLDING.size() < 20) {
             InMemoryStorage.VALUE_HOLDING.add(x);
         } else {
@@ -35,19 +35,20 @@ public class StandardDeviationServiceIml implements StandardDeviationService {
         double sd = calculateStandardDeviation();
         double rate = calculateDataChangeRate(sd);
         InMemoryStorage.PREVIOUS_SD = sd;
-        int result = 0;
+        Integer result = 0;
         try {
             // rate matrix in percentage
             if (rate > 50) {
                 result = 1;
             }
-            kafkaTemplate.send(CHANGE_DETECTED_TOPIC, result);
+            String CHANGE_DETECTED_TOPIC = INPUT_TOPIC.replace("RTDI", "CD");
+            kafkaTemplate.send(CHANGE_DETECTED_TOPIC, result.toString());
+            System.out.println(CHANGE_DETECTED_TOPIC +" " +result);
         } catch (Exception ex) {
             log.info("Error: ", ex);
         }
         log.info("=====> data change result: {}", result);
     }
-
     private Double calculateDataChangeRate(double sd) {
         double diff = Math.abs(sd - InMemoryStorage.PREVIOUS_SD);
         return (diff / InMemoryStorage.PREVIOUS_SD) * 100;
